59af4b07045d1e9815b5b0bb457e5e3c31b622d9
Margarita2113 <rita_2113@mail.ru>
2025-03-09 19:24:26 +0300

    new

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,4 +1,3 @@
-
 import os
 from pathlib import Path
 
@@ -60,12 +59,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME', 'Kitty_db'),
-        'USER': os.getenv('DB_USER', 'rita'), 
-        'PASSWORD': os.getenv('DB_PASSWORD', 'Zaq21412'), 
-        'HOST': os.getenv('DB_HOST', 'postgres'), 
+        'USER': os.getenv('DB_USER', 'rita'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'Zaq21412'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
