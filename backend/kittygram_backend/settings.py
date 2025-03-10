9aa53df6adac330c98e8d2716fb8b58e60840c30
Margarita2113 <rita_2113@mail.ru>
2025-03-10 21:36:18 +0300

    new

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -58,11 +58,11 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'Kitty_db'),
-        'USER': os.getenv('DB_USER', 'rita'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'Zaq21412'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
